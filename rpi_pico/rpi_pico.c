3e37f124312d13aa1ab65476238d71fa1d79f4bf
Stefan Kerkmann <karlk90@pm.me>
2022-09-20 18:23:01 +0200

    Add animation examples by GEIST

--- rpi_pico/rpi_pico.c
+++ rpi_pico/rpi_pico.c
@@ -17,51 +17,74 @@
 #include QMK_KEYBOARD_H
 
 #ifdef QUANTUM_PAINTER_ENABLE
-#    include "qp.h"
-#    include "karlk90.qgf.h"
-#    include "iosevka11.qff.h"
+#include "animation.qgf.h"
+#include "instaclip_16colors.qgf.h"
+#include "iosevka11.qff.h"
+#include "karlk90.qgf.h"
+#include "qp.h"
 
 #define LCD_CS_PIN GP17
 #define LCD_DC_PIN GP16
 #define LCD_RST_PIN GP20
-static painter_device_t       display;
+
+static painter_device_t display;
 static painter_image_handle_t image;
-static painter_font_handle_t  font;
+static painter_font_handle_t font;
+static painter_image_handle_t animation;
+static deferred_token animation_token;
 #endif
 
 void keyboard_post_init_user(void) {
-    debug_enable = true;
+  debug_enable = true;
 #ifdef QUANTUM_PAINTER_ENABLE
-    display = qp_st7789_make_spi_device(240, 240, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 2, 3);
-    // display = qp_ili9341_make_spi_device(240, 320, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 2, 0);
-    qp_init(display, QP_ROTATION_0); // Initialise the display
-    qp_power(display, true);
+  display = qp_st7789_make_spi_device(240, 320, LCD_CS_PIN, LCD_DC_PIN,
+                                      LCD_RST_PIN, 1, 3);
+  // display = qp_ili9341_make_spi_device(240, 320, LCD_CS_PIN, LCD_DC_PIN,
+  // LCD_RST_PIN, 2, 0);
+  qp_init(display, QP_ROTATION_0); // Initialise the display
+  qp_power(display, true);
+  qp_rect(display, 0, 0, 239, 319, 0, 0, 0, true);
+#if 1
+
+#if 1
+  animation = qp_load_image_mem(gfx_instaclip_16colors);
+#else
+  animation = qp_load_image_mem(gfx_animation);
+#endif
+
+  if (animation != NULL) {
+    animation_token = qp_animate(display, (240 - animation->width),
+                                 (240 - animation->height), animation);
+  }
+#endif
+
+#if 0
     image = qp_load_image_mem(gfx_karlk90);
     if (image != NULL) {
         qp_drawimage(display, 0, 0, image);
         qp_flush(display);
     }
-    font = qp_load_font_mem(font_iosevka11);
-    if (font != NULL) {
-        qp_drawtext(display, 40, 160, font, "QUANTUM PAINTER @ RP2040");
-    }
+  font = qp_load_font_mem(font_iosevka11);
+  if (font != NULL) {
+    qp_drawtext(display, 40, 160, font, "QUANTUM PAINTER @ RP2040");
+  }
 #endif
-    // Customise these values to desired behaviour
-    // debug_matrix = true;
-    // debug_keyboard = true;
-    // debug_mouse=true;
+
+#endif
 }
 
 void housekeeping_task_user(void) {
-    static uint32_t last_draw = 0;
-    if (timer_elapsed32(last_draw) > 33) { // Throttle to 30fps
-        last_draw = timer_read32();
-        // Draw 8px-wide rainbow down the left side of the display
-        for (int i = 240; i < 320; ++i) {
-            qp_line(display, 0, i, 239, i, i, 255, 255);
-        }
-        qp_flush(display);
+#if 0
+  static uint32_t last_draw = 0;
+  if (timer_elapsed32(last_draw) > 33) { // Throttle to 30fps
+    last_draw = timer_read32();
+    // Draw 8px-wide rainbow down the left side of the display
+    for (int i = 240; i < 320; ++i) {
+      qp_line(display, 0, i, 239, i, i, 255, 255);
     }
+    qp_flush(display);
+  }
+#endif
 }
 
 void board_init(void) {}
--- rpi_pico/rules.mk
+++ rpi_pico/rules.mk
@@ -4,17 +4,20 @@ BOOTLOADER = rp2040
 
 ALLOW_WARNINGS = yes
 PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS!
-VIA_ENABLE = yes
+VIA_ENABLE = no
 #RAW_ENABLE = no
 SHARED_EP_ENABLE = no
 CONSOLE_ENABLE = yes
 LTO_ENABLE = yes
-OPT_DEFS = -Os -gdwarf-4# -DUSB_DEBUG
+OPT_DEFS = -O2 
 
-SRC     += karlk90.qgf.c iosevka11.qff.c
+SRC += karlk90.qgf.c
+SRC += animation.qgf.c
+SRC += instaclip_16colors.qgf.c
+SRC += iosevka11.qff.c
 
 QUANTUM_PAINTER_ENABLE             = yes
-QUANTUM_PAINTER_DRIVERS            += ili9341_spi st7789_spi
+QUANTUM_PAINTER_DRIVERS            += ili9341_spi st7789_spi rgb565_surface
 DEBUG_MATRIX_SCAN_RATE_ENABLE = yes
 
 LAYOUTS = ortho_1x1
