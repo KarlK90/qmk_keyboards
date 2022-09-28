c7458f6206408746198cb726b18a26c4dfec0f84
Stefan Kerkmann <stefan.kerkmann@rhebo.com>
2022-09-28 15:19:30 +0200

    enable rgb light via kb2040 ws2812 led

--- klor/config.h
+++ klor/config.h
@@ -65,7 +65,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* rgb led support */
-#define RGB_DI_PIN D3
+#define RGB_DI_PIN 17U
 #define RGBLED_NUM 42
 #define RGBLED_SPLIT { 21, 21 }
 #define RGBLIGHT_ANIMATIONS
@@ -108,4 +108,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGB_MATRIX_KEYPRESSES
 #endif
-
